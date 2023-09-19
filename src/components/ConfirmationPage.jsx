119f9b16e350a5cfc845af0d24838392700a92bb
Alice Lin <alicexxlin@gmail.com>
2023-09-19 14:09:39 -0600

    Confirmation Page

---
+++ src/components/ConfirmationPage.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import OrderConfirmation from "./OrderConfirmation";
+
+function ConfirmationPage() {
+  const [order, setOrder] = useState(null);
+  const { id } = useParams();
+
+  const getOrder = async () => {
+    const response = await fetch(`http://localhost:3001/api/orders/${id}`);
+    const data = await response.json();
+    setOrder(data);
+  };
+
+  useEffect(() => {
+    getOrder();
+  }, [id]);
+
+  return (
+    <div>{order ? <OrderConfirmation order={order} /> : <p>Loading...</p>}</div>
+  );
+}
+
+export default ConfirmationPage;
