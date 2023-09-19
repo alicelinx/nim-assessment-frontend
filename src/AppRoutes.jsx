9b66151b313d74f04b309c2b9d65dded08b458be
Alice Lin <alicexxlin@gmail.com>
2023-09-19 13:58:02 -0600

    Revert "Confirmation Page"

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
-import ConfirmationPage from "./components/ConfirmationPage";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/order-confirmation/:id" element={<ConfirmationPage />} />
     </Routes>
   );
 }
